1ca90b67850a762c41be354d1179a8c85b175960
Kudoa1 <Kudoagameryt@gmail.com>
2024-09-20 09:42:48 -0300

    Post Zona

--- src/main/java/com/ahk/arg/forsale/controllers/ZonasController.java
+++ src/main/java/com/ahk/arg/forsale/controllers/ZonasController.java
@@ -3,8 +3,11 @@ package com.ahk.arg.forsale.controllers;
 import com.ahk.arg.forsale.models.entities.Zona;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,4 +49,17 @@ public class ZonasController {
         return new ModelAndView("agregarZona"); // nombre del archivo HTML para el formulario
     }
 
+    //guardar Zona
+    @PostMapping("/guardar")
+    //opcion 1: devolver una vista de exito al crear la zona (sin redirigir ni hacer get, solo LEVANTA)
+    public ModelAndView guardarZona(@ModelAttribute Zona zona, Map<String, Object> model){
+        this.zonas.add(zona);   //agrego la zona a mi coleccion de zonas
+        model.put("mensajeExito","Se guardó la zona con éxito!");
+        // levantar la vista de exito (sin hacer get)
+        return new ModelAndView("zonas_exito",model);
+    }
+    //opcion 2: Redirreccionar a una vista
+    public RedirectView guardarZona(){
+        return new RedirectView("/zonas");
+    }
 }
